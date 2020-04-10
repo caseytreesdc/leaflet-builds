e7e26087c2d88869aa83d4a8848bb97d75d0bd55
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-04-10 13:01:57 -0400

    4-13-20 press4-13-20 press4-13-20 press4-13-20 press4-13-20 press4-13-20 press4-13-20 press4-13-20 press4-13-20 press4-13-20 press4-13-20 press4-13-20 press4-13-20 press

--- streamreader.py
+++ streamreader.py
@@ -1,35 +1,35 @@
 import sys
 
 
-NVP_20200406 = {
+NVP_20200413 = {
     'preheader': 'Like our new leaflet look?',
-    'article-1__link': 'https://caseytrees.org/2020/04/its-the-final-countdown/?utm_source=leaflet&utm_medium=email&utm_campaign=spring',
+    'article-1__link': 'https://caseytrees.org/2020/04/for-the-future/?utm_source=leaflet&utm_medium=email&utm_campaign=membership',
     'article-1__alt-text': '#blossombracket finals!',
     'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/cherry-blossom-bracket-article-one.png',
     'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/04/bracket-mobile.png',
-    'article-1__heading': 'It’s the Final Countdown...',
-    'article-1__description': '...of our Cherry Blossom Bracket. Find out who the top two are and vote for your favorite!',
-    'button-1__text': 'Virtual Classes!',
-    'button-1__link': '',
-    'button-2__text': 'Your City Your Trees',
-    'button-2__link': '',
-    'article-2__link': 'https://caseytrees.org/2020/04/lets-kick-off-arbor-month-with-a-lil-quiz-on-its-history/?utm_source=leaflet&utm_medium=email&utm_campaign=nationalholiday',
+    'article-1__heading': 'For the future...',
+    'article-1__description': '. ...while finding joy in the present. And what that has to do with trees.',
+    'button-1__text': 'New Virtual Tree ID Class!',
+    'button-1__link': 'https://caseytrees.org/2020/04/virtual-classes-post/?utm_source=leaflet&utm_medium=email&utm_campaign=class',
+    'button-2__text': 'Sign Up Now',
+    'button-2__link': 'https://caseytrees.org/2020/04/virtual-classes-post/?utm_source=leaflet&utm_medium=email&utm_campaign=class',
+    'article-2__link': 'https://caseytrees.org/2020/04/winner-of-cherry-blossom-bracket/?utm_source=leaflet&utm_medium=email&utm_campaign=spring',
     'article-2__alt-text': 'arbor day quiz!',
     'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/citscimonth-article-two.png',
     'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/04/citiscimonth-responsive.jpg',
-    'article-2__heading': ' April is Citizen Science Month',
-    'article-2__description': 'We’re here to tell you all about what citizen science is and why it’s a great activity for our physically distant world these days.',
-    'article-3__link': 'https://caseytrees.org/2020/04/april-is-citizen-science-month/?utm_source=leaflet&utm_medium=email&utm_campaign=citizenscience',
-    'article-3__alt-text': 'april is citizen science month',
+    'article-2__heading': 'Winner of Cherry Blossom Bracket',
+    'article-2__description': 'drumroll please...',
+    'article-3__link': 'https://caseytrees.org/2020/04/weve-added-to-ou…trees-of-d-c-map/?utm_source=leaflet&utm_medium=email&utm_campaign=maps',
+    'article-3__alt-text': 'our maps pics ALT TEXT',
     'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/arbordayquiz-email-browser.jpg',
     'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/04/arbordayquiz-email-mobile.jpg',
-    'article-3__heading': 'Let’s Kick Off Arbor Month with a Lil Quiz on its History',
-    'article-3__description': 'To start, let’s learn the history of Arbor Day and how it came to be.',
-    'button-3__link': 'https://connect.clickandpledge.com/w/Form/b7852b67-4119-4738-bb07-db98ef6eaee4?637139852829640420utm_source=leaflet&utm_medium=email&utm_campaign=leaflet&trk=leaflet',
+    'article-3__heading': 'We’ve Added to Our Blossoming Trees of D.C. Map!',
+    'article-3__description': ' Featuring freshly added favorites like Lindens, Redbuds, and Tulip Trees.',
+    'button-3__link': 'https://connect.clickandpledge.com/w/Form/76e310e2-e233-46b8-9cf6-c6995b4c5d97?trk=arbordayquiz',
     'button-3__text': "DONATE TO HELP SUPPORT D.C.'s TREES!"
 }
 
-NVP = NVP_20200406
+NVP = NVP_20200413
 #
 
 

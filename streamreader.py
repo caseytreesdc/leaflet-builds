897b0b90c2013d8639128405631ec417ebdfab86
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-04-16 16:12:56 -0400

    working on 4.20.20

--- streamreader.py
+++ streamreader.py
@@ -1,8 +1,8 @@
 import sys
 
 
-NVP_20200413 = {
-    'preheader': 'Celebrating the city’s most cherished cherry!',
+NVP_20200420 = {
+    'preheader': 'by sending your friends and loved ones a goofy Arbor Day greeting',
     'article-1__link': 'https://caseytrees.org/2020/04/for-the-future/?utm_source=leaflet&utm_medium=email&utm_campaign=membership',
     'article-1__alt-text': 'kids planting trees',
     'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/for-the-future-browser.jpg',
@@ -17,25 +17,25 @@ NVP_20200413 = {
     'button-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/Leaflet-5k-ad-02-browser.png',
     'button-2__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/04/Leaflet-5k-ad-02-responsive.png',
     'button-2__alt-text': 'Regeister Here - Earth Day Virtual 5k',
-    'article-2__link': 'https://caseytrees.org/2020/04/winner-of-cherry-blossom-bracket/?utm_source=leaflet&utm_medium=email&utm_campaign=spring',
-    'article-2__alt-text': 'blossom bracket winner!',
-    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/cherry-blossom-winner-browser.png',
-    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/04/cherry-blossom-winner-responsive.png',
-    'article-2__heading': 'Winner of Cherry Blossom Bracket ',
-    'article-2__description': 'drumroll please...',
-    'article-3__link': 'https://caseytrees.org/2020/04/weve-added-to-our-blossoming-trees-of-d-c-map/?utm_source=leaflet&utm_medium=email&utm_campaign=membership',
+    'article-2__link': 'https://caseytrees.org/2020/04/spring-cleaning-your-house-and-the-earth/?utm_source=leaflet&utm_medium=email&utm_campaign=treecare,
+    'article-2__alt-text': 'spring cleaning!',
+    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/spring-cleaning-2020-email-browser.png',
+    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/04/spring-cleaning-2020-article-responsive.png',
+    'article-2__heading': 'Spring Cleaning Your House and The Earth',
+    'article-2__description': 'Everything connected, which our Earth (and COVID-19) showcases',
+    'article-3__link': 'https://caseytrees.org/2020/04/celebrating-the-emancipation-oak-this-emancipation-day/?utm_source=leaflet&utm_medium=email&utm_campaign=random',
     'article-3__alt-text': "We've added more trees to our Blossoming Trees of DC.",
-    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/trees-of-note-browser-copy.png',
-    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/04/trees-of-note-update-responsive.png',
-    'article-3__heading': 'We’ve Added to Our Blossoming Trees of D.C. Map!',
-    'article-3__description': ' Featuring freshly added favorites like Lindens, Redbuds, and Tulip Trees.',
+    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/emancipation-oak-article-browser.png',
+    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/04/emancipation-oak-article-responsive.png',
+    'article-3__heading': 'Celebrating the Emancipation Oak this Emancipation Day',
+    'article-3__description': 'A (giant) piece of American history in D.C.’s backyard',
     'banner__link': 'https://connect.clickandpledge.com/w/Form/76e310e2-e233-46b8-9cf6-c6995b4c5d97?trk=arbordayquiz',
     'banner__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/Leaflet-ad.png',
     'banner__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/04/Leaflet-ad-mobile.png',
     'banner__alt-text': 'Plant it Forward',
 }
 
-NVP = NVP_20200413
+NVP = NVP_20200420
 #
 
 

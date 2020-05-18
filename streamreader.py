41a56884c6ed133680fa2254b3f91e34a38fef28
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-05-18 09:45:45 -0400

    leaflet may 18h 2020

--- streamreader.py
+++ streamreader.py
@@ -1,37 +1,37 @@
 import sys
 
 
-NVP_20200511 = {
-    'preheader': 'Discounts on Tree Adoptions continue, plus help collect data!',
-    'article-1__link': 'https://caseytreesdc.github.io/ct-videos/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
-    'article-1__alt-text': 'Casey Trees Remote! ',
-    'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/ct-remote-bowesr.png',
-    'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/ct-remote-responsleeve.png',
-    'article-1__heading': 'New Your City, Your Trees Class & Other Remote Ways to Engage With Us!',
-    'article-1__description': 'For when you are bored in the house and in the house bored.',
+NVP_20200518= {
+    'preheader': 'Plus, see who won the Urban Tree World Cup',
+    'article-1__link': 'https://caseytrees.org/2020/05/go-behind-the-scenes-at-casey-trees-with-tea-trees/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
+    'article-1__alt-text': 'Go Behind the Scenes at Casey Trees with Tea & Trees!',
+    'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/tea-browser.png',
+    'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/tea-responsive.png',
+    'article-1__heading': 'Go Behind the Scenes at Casey Trees with Tea & Trees!',
+    'article-1__description': 'Get to know Casey Trees staff and fun tree facts in our newest, shortest, video series yet.',
     'watering__link': 'https://caseytrees.org/take-action/water/?utm_source=leaflet&utm_medium=email&utm_campaign=wateringalert',
-    'watering__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_mustwater.png',
-    'watering__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_mustwater-responsive.png',
+    'watering__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_optional.png',
+    'watering__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_optional-responsive.png',
     'watering__alt-text': 'Young trees need plenty of water this week.',
-    'article-2__link': 'https://caseytrees.org/2020/05/take-a-survey-on-nature-and-human-well-being-in-times-of-isolation-and-a-global-pandemic/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
-    'article-2__alt-text': 'a survey on nature and our well-being',
-    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/ubc-forestry-survey-browser.png',
-    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/ubc-forestry-survey-responsive.png',
-    'article-2__heading': 'Take a Survey on Nature and Human Well-Being in Times of Isolation and Global Pandemic',
-    'article-2__description': "Help out an old intern's new project!",
-    'article-3__link': 'https://caseytrees.org/2020/05/tree-adoptions-are-still-discounted-for-mothers-day/?utm_source=leaflet&utm_medium=email&utm_campaign=store',
-    'article-3__alt-text': "smell the gingko tree",
-    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/mothers-day-browser.png',
-    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/mothers-day-responsive.png',
-    'article-3__heading': 'Say What You Need to Say! Tree Adoptions Remain Discounted!',
-    'article-3__description': 'Show mom, friends, family, ANYONE how much they mean by sending them a tree adoption that helps plant MORE trees!',
+    'article-2__link': 'https://caseytrees.org/2020/05/2019-tree-report-card-empowering-future-generations-through-trees/?utm_source=leaflet&utm_medium=email&utm_campaign=treereportcard',
+    'article-2__alt-text': 'Empowering future generations through trees.',
+    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/trc-youth-spotlight-browser.png',
+    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/trc-youth-spotlight-responsive.png',
+    'article-2__heading': '2019 Tree Report Card: Empowering Future Generations Through Trees',
+    'article-2__description': "When we teach younger generations about the value of trees and green space now, we will ensure that our city’s lush tree filled legacy continues.",
+    'article-3__link': 'https://caseytrees.org/2020/05/urban-tree-world-cup/?utm_source=leaflet&utm_medium=email&utm_campaign=random',
+    'article-3__alt-text': 'Urban Tree World Cup',
+    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/urban-tree-world-browser.png',
+    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/urban-tree-world-champ-responsive.png',
+    'article-3__heading': 'Urban Tree World Cup',
+    'article-3__description': 'London plane knocked out the Dawn redwood in the knock-out tournament run by the Arboricultural Association during lockdown on Twitter.',
     'banner__link': 'https://connect.clickandpledge.com/w/Form/b7852b67-4119-4738-bb07-db98ef6eaee4?637139852829640420&_ga=2.252713097.1890199852.1589214584-173857063.1587570366?trk=leaflet',
     'banner__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/Leaflet-ad.png',
     'banner__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/04/Leaflet-ad-mobile.png',
     'banner__alt-text': 'Plant it Forward',
 }
 
-NVP = NVP_20200511
+NVP = NVP_20200518
 #
 
 

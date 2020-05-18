a32ef190f2ae94e0e6b594aeb5e97314afb931b5
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-05-18 07:46:57 -0400

    may 11th 2020

--- streamreader.py
+++ streamreader.py
@@ -1,37 +1,37 @@
 import sys
 
 
-NVP_20200427 = {
-    'preheader': '',
-    'article-1__link': 'https://caseytrees.org/2020/05/weekly-watering-alerts-are-here/?utm_source=leaflet&utm_medium=email&utm_campaign=weeklywateringalert',
-    'article-1__alt-text': 'Watering Alerts are here!',
-    'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/watering-alert-browser.jpg',
-    'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/watering-alert-repsonsive.jpg',
-    'article-1__heading': 'Weekly Watering Alerts are Here!',
-    'article-1__description': 'Young trees need 1.5 inches of rain or 25 gallons of water a week. We’ll let you know whether or not to water them!',
-    'watering__link': 'https://caseytrees.org/2020/05/weekly-watering-alerts-are-here/?utm_source=leaflet&utm_medium=email&utm_campaign=weeklywateringalert',
-    'watering__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_optional.png',
-    'watering__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_optional-responsive.png',
-    'watering__alt-text': 'Watering Optional',
-    'article-2__link': 'https://caseytrees.org/2020/05/introducing-our-newest-video-series-branch-out/?utm_source=leaflet&utm_medium=email&utm_campaign=staff',
-    'article-2__alt-text': 'New short videos',
-    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/ct-videos-browser.jpg',
-    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/ct-videos-responsive.jpg',
-    'article-2__heading': 'Branch Out with Casey Trees & Anacostia Riverkeeper',
-    'article-2__description': 'Introducing Our Newest Video Series',
-    'article-3__link': 'https://caseytrees.org/2020/05/d-c-launches-its-annual-campaign-against-the-smell-of-the-ginkgo-tree/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
+NVP_20200511 = {
+    'preheader': 'Discounts on Tree Adoptions continue, plus help collect data!',
+    'article-1__link': 'https://caseytreesdc.github.io/ct-videos/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
+    'article-1__alt-text': 'Casey Trees Remote! ',
+    'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/ct-remote-bowesr.png',
+    'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/ct-remote-responsleeve.png',
+    'article-1__heading': 'New Your City, Your Trees Class & Other Remote Ways to Engage With Us!',
+    'article-1__description': 'For when you are bored in the house and in the house bored.',
+    'watering__link': 'https://caseytrees.org/take-action/water/?utm_source=leaflet&utm_medium=email&utm_campaign=wateringalert',
+    'watering__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_mustwater.png',
+    'watering__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_mustwater-responsive.png',
+    'watering__alt-text': 'Young trees need plenty of water this week.',
+    'article-2__link': 'https://caseytrees.org/2020/05/take-a-survey-on-nature-and-human-well-being-in-times-of-isolation-and-a-global-pandemic/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
+    'article-2__alt-text': 'a survey on nature and our well-being',
+    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/ubc-forestry-survey-browser.png',
+    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/ubc-forestry-survey-responsive.png',
+    'article-2__heading': 'Take a Survey on Nature and Human Well-Being in Times of Isolation and Global Pandemic',
+    'article-2__description': "Help out an old intern's new project!",
+    'article-3__link': 'https://caseytrees.org/2020/05/tree-adoptions-are-still-discounted-for-mothers-day/?utm_source=leaflet&utm_medium=email&utm_campaign=store',
     'article-3__alt-text': "smell the gingko tree",
-    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/ginkgo-spray-browser.png',
-    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/ginkgo-spray-responsive.png',
-    'article-3__heading': 'D.C. Launches its Annual Campaign Against the Smell of the Ginkgo Tree',
-    'article-3__description': 'Spraying began April 20 and will continue at night. Residents don’t have to move their cars or do anything else.',
-    'banner__link': 'https://connect.clickandpledge.com/w/Form/76e310e2-e233-46b8-9cf6-c6995b4c5d97?trk=arbordayquiz',
+    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/mothers-day-browser.png',
+    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/05/mothers-day-responsive.png',
+    'article-3__heading': 'Say What You Need to Say! Tree Adoptions Remain Discounted!',
+    'article-3__description': 'Show mom, friends, family, ANYONE how much they mean by sending them a tree adoption that helps plant MORE trees!',
+    'banner__link': 'https://connect.clickandpledge.com/w/Form/b7852b67-4119-4738-bb07-db98ef6eaee4?637139852829640420&_ga=2.252713097.1890199852.1589214584-173857063.1587570366?trk=leaflet',
     'banner__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/Leaflet-ad.png',
     'banner__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/04/Leaflet-ad-mobile.png',
     'banner__alt-text': 'Plant it Forward',
 }
 
-NVP = NVP_20200427
+NVP = NVP_20200511
 #
 
 
@@ -48,4 +48,3 @@ def replacekeys(line):
 for line in sys.stdin:
     # stdout is redirected to > filename.html
     print(replacekeys(line), end='')
-

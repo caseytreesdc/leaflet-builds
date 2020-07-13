4e939d99df0829ae8843e33f32330e72689c4728
Tissa Khosla <tissakhosla@tissas-mbp.myfiosgateway.com>
2020-07-13 12:11:06 -0400

    july 13 2020 leafl pre MB edits

--- streamreader.py
+++ streamreader.py
@@ -1,37 +1,37 @@
 import sys
 
 
-NVP_20200518 = {
-    'preheader': 'Plus, Your Favorite Hidden Urban Oasis is Open!',
-    'article-1__link': 'https://caseytrees.org/2020/06/lets-talk-about-urban-island-effect/?utm_source=leaflet&utm_medium=email&utm_campaign=research',
-    'article-1__alt-text': 'The Urban Island Heat Effect',
-    'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/06/UHI-email-browser.png',
-    'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/06/UHI-email.png',
-    'article-1__heading': 'Let’s Talk About Urban Island Effect',
-    'article-1__description': 'And its unsurprising twist.',
+NVP_20200713 = {
+    'preheader': 'and a look into the green infrastructure at our HQ',
+    'article-1__link': 'https://caseytrees.org/2020/07/did-you-see-popvilles-list-of-the-best-park-garden-small-forest-in-dc-to-sit-and-read-all-day/?utm_source=leaflet&utm_medium=email&utm_campaign=mediamention',
+    'article-1__alt-text': '',
+    'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/07/popville-getaway-300.png',
+    'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/07/popville-getaway-email.png',
+    'article-1__heading': 'Did you see PoPville’s list of the “best park/garden/small forest in DC to sit and read all day?”',
+    'article-1__description': 'Do you have any additions?',
     'watering__link': 'https://caseytrees.org/take-action/water/?utm_source=leaflet&utm_medium=email&utm_campaign=wateringalert',
     'watering__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_mustwater.png',
     'watering__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_mustwater-responsive.png',
     'watering__alt-text': 'Young trees need plenty of water this week.',
-    'article-2__link': 'https://caseytrees.org/2020/06/the-arboretums-open/?utm_source=leaflet&utm_medium=email&utm_campaign=city-partner',
-    'article-2__alt-text': 'the arboretum is open!',
-    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/06/UNA-email-browser.png',
-    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/06/UNA-email.png',
-    'article-2__heading': "The Arboretum’s Open!",
-    'article-2__description': "Wide open spaces, unusual trees, and plenty of sunshine and fresh air.",
-    'article-3__link': 'https://caseytrees.org/2020/06/color-coding-demystifying-city-spray-paint-marks-2/?utm_source=leaflet&utm_medium=email&utm_campaign=ddot-ufa',
-    'article-3__alt-text': 'Demystifying City Spray Paint Marks',
-    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/06/spray-paint-email-browser.jpg',
-    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/06/spray-paint-email-responsive.jpg',
-    'article-3__heading': 'Color Coding: Demystifying City Spray Paint Marks',
-    'article-3__description': "What does that orange dot on a tree mean?",
-    'banner__link': 'https://www.tfaforms.com/4826832',
-    'banner__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/leaflet-youth-programs-2020.png',
-    'banner__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/05/leaflet-youth-programs-2020.png',
-    'banner__alt-text': 'youth programs survey',
+    'article-2__link': 'https://caseytrees.org/2020/07/17-straight-days-above-90-degrees-water-your-trees/?utm_source=leaflet&utm_medium=email&utm_campaign=treecare',
+    'article-2__alt-text': '',
+    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/04/water-email.jpg',
+    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/04/water-resplonsive.jpg',
+    'article-2__heading': '17 Straight Days Above 90 Degrees = Water Your Trees!',
+    'article-2__description': "Yes, we recommend watering even though there have been downpours. Consistent, slow watering is key!",
+    'article-3__link': 'https://caseytrees.org/2020/07/green-features-at-casey-trees-hq/?utm_source=leaflet&utm_medium=email&utm_campaign=rsh',
+    'article-3__alt-text': '',
+    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/07/caseytrees-hq-160.png',
+    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/07/caseytrees-hq-email.png',
+    'article-3__heading': 'Green Features at Casey Trees HQ',
+    'article-3__description': 'Did you know our headquarters in Brookland features a fruit bearing rain garden and a green roof?',
+    'banner__link': 'https://caseytrees.org/farm',
+    'banner__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/07/Leaflet-ad_farm.png',
+    'banner__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/07/Leaflet-ad_farm-responsive.png',
+    'banner__alt-text': '',
 }
 
-NVP = NVP_20200518
+NVP = NVP_20200713
 #
 
 

7fae54da9273bae8b2552c3efade9324dc78384f
Tissa Khosla <tissakhosla@tissas-mbp.myfiosgateway.com>
2020-08-31 17:25:00 -0400

    8-31-20

--- streamreader.py
+++ streamreader.py
@@ -1,37 +1,37 @@
 import sys
 
 
-NVP_20200817 = {
-    'preheader': 'We’re celebrating National Dog Day with a complete canine collection of stories!',
-    'article-1__link': 'https://caseytrees.org/2020/08/canopy-cover-at-dog-parks-in-dc/?utm_source=leaflet&utm_medium=email&utm_campaign=gis',
-    'article-1__alt-text': 'a lab puppy!',
-    'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/08/dogparks-browser.png',
-    'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/08/dogparks-email.png',
-    'article-1__heading': 'Canopy Cover at Dog Parks in DC',
-    'article-1__description': 'Counting down the top 10 puppy play places.',
+NVP_20200831 = {
+    'preheader': 'A book recommendation, a virtual annual meeting, and more exciting, pandemic friendly activities.',
+    'article-1__link': 'https://caseytrees.org/2020/08/mac-isa-2020-annual-meeting/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
+    'article-1__alt-text': '',
+    'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/08/MAC-ISA-browser.png',
+    'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/08/MAC-ISA-email.png',
+    'article-1__heading': 'MAC-ISA 2020 Annual Meeting ',
+    'article-1__description': 'Take advantage of varied and creative programming, CEUs, and networking opportunities during a pandemic.',
     'watering__link': 'https://caseytrees.org/take-action/water/?utm_source=leaflet&utm_medium=email&utm_campaign=wateringalert',
-    'watering__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_mustwater.png',
-    'watering__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_mustwater-responsive.png',
-    'watering__alt-text': 'Even with the bit of rain, trees need more water this week.',
-    'article-2__link': 'https://caseytrees.org/2020/08/help-trees-survive-the-dog-days-of-summer/?utm_source=leaflet&utm_medium=email&utm_campaign=treecare',
+    'watering__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_optional.png',
+    'watering__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_optional-responsive.png',
+    'watering__alt-text': 'We had some rain, water if you need, but trees are hydrated!.',
+    'article-2__link': 'https://caseytrees.org/2020/08/black-woman-in-green/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
     'article-2__alt-text': '',
-    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/08/dogdays-browser.png',
-    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/08/dogdays-email.png',
-    'article-2__heading': 'Help Trees Survive the Dog Days of Summer',
-    'article-2__description': 'Tips on how to help trees make it despite prolonged heat, short and intense rainfall and even lawn maintenance equipment.',
-    'article-3__link': 'https://caseytrees.org/2020/08/popular-dogs-as-trees-that-we-can-plant-in-your-yard-for-free/?utm_source=leaflet&utm_medium=email&utm_campaign=residential',
-    'article-3__alt-text': '',
-    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/08/breedsastrees-browser.png',
-    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/08/breedsastrees-email.png',
-    'article-3__heading': 'Popular Dogs as Trees (that we can plant in your yard for free!)',
-    'article-3__description': 'Which DC tree represents a golden retriever? Or a Corgi?',
+    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/08/BWING-browser.png',
+    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/08/BWING-email.png',
+    'article-2__heading': 'Black Woman in Green',
+    'article-2__description': 'Marking the passage of the 19th Amendment, while learning more about our own field.',
+    'article-3__link': 'https://caseytrees.org/2020/08/a-new-era-on-12th-street-ne/?utm_source=leaflet&utm_medium=email&utm_campaign=random',
+    'article-3__alt-text': 'Gloria Brown and the Inmarked Trail to Forest Service LEadership',
+    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/08/annex-browser.png',
+    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/08/annex-email.png',
+    'article-3__heading': 'A New Era on 12th Street NE',
+    'article-3__description': 'Looking back before looking forward.',
     'banner__link': 'https://casey-trees-dc.square.site/product/face-mask-dc-flag/87?cs=true',
     'banner__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/08/Mask_Ad_1.png',
     'banner__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/08/MaskAd2-responsive.png',
     'banner__alt-text': 'Get a Casey Trees Mask! Protect Yourself, Protect others, Protect Our Urban Forester',
 }
 
-NVP = NVP_20200817
+NVP = NVP_20200831
 #
 
 

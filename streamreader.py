c68488aa1e2878ea6e9197ee9ba871a5bc412540
TKhosla <tkhosla@caseytrees.org>
2020-10-13 08:15:27 -0400

    10-13-2010-13-2010-13-2010-13-2010-13-2010-13-2010-13-2010-13-2010-13-2010-13-20

--- streamreader.py
+++ streamreader.py
@@ -1,37 +1,37 @@
 import sys
 
 
-NVP_20200914 = {
-    'preheader': 'Where to find, and how to identify, beloved fall foliage right outside your door!',
-    'article-1__link': 'https://caseytrees.org/2020/09/a-frontline-fall-forecast/?utm_source=leaflet&utm_medium=email&utm_campaign=staff',
+NVP_20201005 = {
+    'preheader': 'and happy Oaktober!',
+    'article-1__link': 'https://caseytrees.org/2020/10/take-our-dc-parks-challenge/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
     'article-1__alt-text': '',
-    'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/09/fall-colors-browser.png',
-    'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/09/fall-colors-email.png',
-    'article-1__heading': 'We Updated our Fall Colors Map',
-    'article-1__description': 'With data you can do a lot of things, but we made a Fall Color map to help you make the most of fall foliage without even leaving the city - or your neighborhood!',
-    'watering__link': 'https://caseytrees.org/2020/09/we-updated-our-fall-colors-map/?utm_source=leaflet&utm_medium=email&utm_campaign=maps',
-    'watering__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_mustwater.png',
-    'watering__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/05/WWA_mustwater-responsive.png',
-    'watering__alt-text': 'We had some rain, but our trees still need water!.',
-    'article-2__link': 'https://caseytrees.org/2020/09/tree-dedications-are-still-on-for-fall/?utm_source=leaflet&utm_medium=email&utm_campaign=dedication',
+    'article-1__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/10/ready2play-browser.png',
+    'article-1__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/09/ready2play-email.png',
+    'article-1__heading': 'Take our DC Parks Challenge',
+    'article-1__description': 'Help Plan the Future of DC Parks!',
+    'watering__link': 'https://caseytrees.maps.arcgis.com/apps/webappviewer/index.html?id=c7c2a30d5c95440b8316b12c7add426d',
+    'watering__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/10/Leaflet-ad_fall-color-map.png',
+    'watering__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/10/Leaflet-ad_fall-color-map-mobile.png',
+    'watering__alt-text': 'View our fall color map.',
+    'article-2__link': 'https://caseytrees.org/2020/10/feelin-hot-hot-hot-2020-weekly-watering-alert-wrap-up/?utm_source=leaflet&utm_medium=email&utm_campaign=weeklywateringalert',
     'article-2__alt-text': '',
-    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/09/member-callout-browsers.png',
-    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/09/member-callout-email.png',
-    'article-2__heading': 'Tree Dedications are Still on for Fall',
-    'article-2__description': 'An opportunity for you to plant a tree in honor of a loved one',
-    'article-3__link': 'https://caseytrees.org/2020/09/viva-los-arboles/?utm_source=leaflet&utm_medium=email&utm_campaign=nationalholiday',
+    'article-2__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/10/wwa-wrapup-2020-browser.png',
+    'article-2__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/09/wwa-wrapup-2020-email.png',
+    'article-2__heading': 'Feelin’ Hot, Hot, Hot: 2020 Weekly Watering Alert Wrap Up',
+    'article-2__description': 'TL;DR there was so much watering this summer.',
+    'article-3__link': 'https://caseytrees.org/2020/10/oaktober-bur-oak/?utm_source=leaflet&utm_medium=email&utm_campaign=riversmarthomes',
     'article-3__alt-text': '',
-    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/09/hispanic-heritage-browser.png',
-    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/09/hispanic-heritage-email.png',
-    'article-3__heading': 'Viva los Árboles',
-    'article-3__description': 'From September 15 to October 15, National Hispanic Heritage Month celebrates the histories, cultures, and contributions of our ancestors from Spain, Mexico, the Caribbean and the Americas',
+    'article-3__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/10/bur-oak-browser.png',
+    'article-3__image-mobile': 'https://caseytrees.org/wp-content/uploads/2020/09/bur-oak-email.png',
+    'article-3__heading': 'Oaktober: Bur Oak',
+    'article-3__description': 'We’re celebrating Oak-tober by highlighting some of the Oak species we can plant for free as part of our residential planting programs.',
     'banner__link': 'https://casey-trees-dc.square.site/product/face-mask-dc-flag/87?cs=true',
     'banner__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/08/Mask_Ad_1.png',
     'banner__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/08/MaskAd2-responsive.png',
     'banner__alt-text': 'Get a Casey Trees Mask! Protect Yourself, Protect others, Protect Our Urban Forester',
 }
 
-NVP = NVP_20200914
+NVP = NVP_20201005
 #
 
 

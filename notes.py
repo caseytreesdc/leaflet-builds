6c6e51c816d99da9b3e475607d44608e79994160
TKhosla <tkhosla@caseytrees.org>
2021-03-12 15:06:45 -0500

    03-08-delimitied

--- notes.py
+++ notes.py
@@ -1137,3 +1137,35 @@ NVP_20210222 = {
 }
 
 NVP = NVP_20210222
+
+
+NVP_20210301 = {
+    'preheader': 'Some of these trees are even blooming right now!',
+    'article-1__link': 'https://caseytrees.org/2021/03/find-spring-blooms-near-you-with-our-blooming-tree-map/?utm_source=leaflet&utm_medium=email&utm_campaign=maps',
+    'article-1__alt-text': 'Current blooms',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2020/04/trees-of-note-update-responsive.png',
+    'article-1__heading': 'Find Spring Blooms Near You with Our Blooming Tree Map',
+    'article-1__description': 'When you open your eyes to all the beautiful blooms DC’s trees offer, you start to see that there is so much more than those delicate white flowers of the Yoshino cherry.',
+    'banner-a__link': 'https://www.eventbrite.com/e/native-trees-for-your-garden-and-neighborhood-tickets-141387982261',
+    'banner-a__alt-text': 'Register for Native Trees, Presented by Capital Nature',
+    'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_capital-nature-event.png',
+    'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_capital-nature-event-mobile.png',
+    'banner-a__text-1': 'REGISTER FOR NATIVE TREES:', 
+    'banner-a__text-2': 'PRESENTED BY CAPTIAL NATURE', 
+    'article-2__link': 'https://caseytrees.org/2021/03/tree-rainbow-red-and-orange-blooms/?utm_source=leaflet&utm_medium=email&utm_campaign=riversmarthomes',
+    'article-2__alt-text': 'more blooms',
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/02/rainbow-red-maple-email.png',
+    'article-2__heading': 'Tree Rainbow: Red and Orange Blooms',
+    'article-2__description': 'Bring the rainbow to your yard with our free trees!',
+    'article-3__link': 'https://caseytrees.org/2021/03/trees-on-structure-and-amazons-hq2/?utm_source=leaflet&utm_medium=email&utm_campaign=store',
+    'article-3__alt-text': 'Trees on structures',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/02/HQ2-email.png',
+    'article-3__heading': 'Trees on Structures and Amazon’s HQ2',
+    'article-3__description': 'We’re way ahead of you Bezos. ',
+    'banner-b__link': 'https://casey-trees.myshopify.com/',
+    'banner-b__alt-text': 'Browse our new store',
+    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_browse-our-new-store.png',
+    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_browse-our-new-store-mobile.png',
+    'banner-b__text-1': 'STORE',
+    'banner-b__text-2': 'STORE',
+}
--- streamreader.py
+++ streamreader.py
@@ -1,38 +1,38 @@
 import sys
 
 
-NVP_20210301 = {
-    'preheader': 'Some of these trees are even blooming right now!',
-    'article-1__link': 'https://caseytrees.org/2021/03/find-spring-blooms-near-you-with-our-blooming-tree-map/?utm_source=leaflet&utm_medium=email&utm_campaign=maps',
-    'article-1__alt-text': 'Current blooms',
-    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2020/04/trees-of-note-update-responsive.png',
-    'article-1__heading': 'Find Spring Blooms Near You with Our Blooming Tree Map',
-    'article-1__description': 'When you open your eyes to all the beautiful blooms DC’s trees offer, you start to see that there is so much more than those delicate white flowers of the Yoshino cherry.',
-    'banner-a__link': 'https://www.eventbrite.com/e/native-trees-for-your-garden-and-neighborhood-tickets-141387982261',
-    'banner-a__alt-text': 'Register for Native Trees, Presented by Capital Nature',
-    'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_capital-nature-event.png',
-    'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_capital-nature-event-mobile.png',
-    'banner-a__text-1': 'REGISTER FOR NATIVE TREES:', 
-    'banner-a__text-2': 'PRESENTED BY CAPTIAL NATURE', 
-    'article-2__link': 'https://caseytrees.org/2021/03/tree-rainbow-red-and-orange-blooms/?utm_source=leaflet&utm_medium=email&utm_campaign=riversmarthomes',
+NVP_20210305 = {
+    'preheader': "#PeakBloom will be here before you know it too",
+    'article-1__link': 'https://caseytrees.org/2021/03/brood-x-is-on-its-way/?utm_source=leaflet&utm_medium=email&utm_campaign=phenology',
+    'article-1__alt-text': '2 Cicadas on a branch',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/02/brood-x-cicada-email.png',
+    'article-1__heading': 'Brood X is on its way',
+    'article-1__description': 'You get a front row seat to one of nature’s most remarkable phenomenons',
+    'banner-a__link': '',
+    'banner-a__alt-text': "Happy International Women's Day",
+    'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_canopy-awards-save-the-date-teaser-v2.png',
+    'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_canopy-awards-save-the-date-teaser-mobile-v2.png',
+    'banner-a__text-1': 'Foresters to Recognize!', 
+    'banner-a__text-2': "INTERNATIONAL WOMEN'S DAY 2021", 
+    'article-2__link': 'https://caseytrees.org/2021/03/nps-predicts-an-april-peak-bloom/?utm_source=leaflet&utm_medium=email&utm_campaign=phenology',
     'article-2__alt-text': 'more blooms',
-    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/02/rainbow-red-maple-email.png',
-    'article-2__heading': 'Tree Rainbow: Red and Orange Blooms',
-    'article-2__description': 'Bring the rainbow to your yard with our free trees!',
-    'article-3__link': 'https://caseytrees.org/2021/03/trees-on-structure-and-amazons-hq2/?utm_source=leaflet&utm_medium=email&utm_campaign=store',
-    'article-3__alt-text': 'Trees on structures',
-    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/02/HQ2-email.png',
-    'article-3__heading': 'Trees on Structures and Amazon’s HQ2',
-    'article-3__description': 'We’re way ahead of you Bezos. ',
-    'banner-b__link': 'https://casey-trees.myshopify.com/',
-    'banner-b__alt-text': 'Browse our new store',
-    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_browse-our-new-store.png',
-    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_browse-our-new-store-mobile.png',
-    'banner-b__text-1': 'STORE',
-    'banner-b__text-2': 'STORE',
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/03/peak-bloom-email.png',
+    'article-2__heading': 'So is Peak Bloom',
+    'article-2__description': 'You get a front row seat to another one of our city’s most remarkable phenomenons',
+    'article-3__link': 'https://caseytrees.org/2021/03/tree-rainbow-yellow-and-green-blooms/?utm_source=leaflet&utm_medium=email&utm_campaign=riversmarthomes',
+    'article-3__alt-text': 'yellow and green blooms',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/03/tuliptree-email.png',
+    'article-3__heading': 'Tree Rainbow: Yellow and Green Blooms',
+    'article-3__description': 'Bring the rainbow to your yard with our free trees!',
+    'banner-b__link': 'https://caseytrees.org/2017/03/run-world-famous-female-foresters/',
+    'banner-b__alt-text': '',
+    'banner-b__image-browser': '',
+    'banner-b__image-responsive': '',
+    'banner-b__text-1': 'Foresters to Recognize!',
+    'banner-b__text-2': "INTERNATIONAL WOMEN'S DAY 2021",
 }
 
-NVP = NVP_20210301
+NVP = NVP_20210305
 #
 
 

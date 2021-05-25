9b9b04618d70432cb140e51877d9de3f8640127b
TKhosla <tkhosla@caseytrees.org>
2021-05-25 05:56:22 -0400

    may 24 2021

--- streamreader.py
+++ streamreader.py
@@ -1,44 +1,38 @@
 import sys
 
 
-NVP_20210510 = {
-    'preheader': "and the top 10 trees of 2020!",
-    'article-1__link': 'https://caseytrees.org/2021/05/celebrating-asian-american-and-pacific-islander-aapi-heritage-month-through-cherry-trees/?utm_source=leaflet&utm_medium=email&utm_campaign=nationalholiday',
-    'article-1__alt-text': 'the "Japanese Lantern" statue at the Tidal Basin, lighted during the Cherry Blossom festival.',
-    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/05/aapi-cherry-blossoms-email.png',
-    'article-1__heading': 'Celebrating Asian American and Pacific Islander (AAPI) Heritage Month through Cherry Trees',
-    'article-1__description': 'Our city’s claim to fame is thanks to a Japanese gift of friendship',
-    # 'banner-a__link': 'https://caseytrees.org/2021/05/cicada-resources/?utm_source=leaflet&utm_medium=email&utm_campaign=treecare',
-    # 'banner-a__alt-text': 'Weekly Watering Alert: Water and Cicada Resources',
-    # 'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/05/2021-Watering-Alerts_must-water-and-cicada.png',
-    # 'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/05/2021-Watering-Alerts_must-water-and-cicada.png',
-    # 'banner-a__text-1': 'Weekly Watering Alert: Water',
-    # 'banner-a__text-2': 'Cicada Resources',
+NVP_20210524 = {
+    'preheader': 'and Essential Tree Care Items.',
+    'article-1__link': 'https://caseytrees.org/2021/05/fostering-stewardship-on-kingman-heritage-islands/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
+    'article-1__alt-text': 'filming with our education team at Kingman and Heritage Islands',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/05/css-kingman-videos-email.png',
+    'article-1__heading': 'Fostering Stewardship on Kingman & Heritage Islands',
+    'article-1__description': 'Our work on the Community Stormwater Solutions Grant and our work collaborating to improve  our waterways',
     'banner-ss__link-left' : 'https://caseytrees.org/take-action/water/',
     'banner-ss__alt-left' : 'Weekly Watering Alerts: Must Water',
     'banner-ss__image-left' : 'https://caseytrees.org/wp-content/uploads/2021/05/weekly-watering-alert_must-water-mobile.jpg',
     'banner-ss__link-right' : 'https://caseytrees.org/2021/05/cicada-resources/?utm_source=leaflet&utm_medium=email&utm_campaign=treecare',
     'banner-ss__alt-right' : 'Cicada Updates',
     'banner-ss__image-right' : 'https://caseytrees.org/wp-content/uploads/2021/05/cicada_leaflet.png',
-    'article-2__link': 'https://caseytrees.org/2021/05/what-are-dcs-trees-worth/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
-    'article-2__alt-text': 'The Washington Monument across the water with someone wake boarding by.',
-    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2020/08/intl-day-of-forests-19-email.png',
-    'article-2__heading': 'What are DC’s Trees Worth?',
-    'article-2__description': 'The literal, monetary value of trees and some quantifiable impacts',
-    'article-3__link': 'https://caseytrees.org/2021/05/top-10-most-wanted-trees/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
-    'article-3__alt-text': 'flowering residential tree in front of row houses',
-    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/05/top-ten-residential-trees-email.png',
-    'article-3__heading': 'Top 10 Most Wanted Trees',
-    'article-3__description': 'The good kind! Here are our most popularly planted trees of the past year',
-    'banner-b__link': 'https://caseytreesdc.github.io/treereportcard2020/',
+    'article-2__link': 'https://caseytrees.org/2021/05/essential-tree-care-items/?utm_source=leaflet&utm_medium=email&utm_campaign=treecare',
+    'article-2__alt-text': 'Watering a young tree with a big bucket!',
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2020/04/water-resplonsive.jpg',
+    'article-2__heading': 'Essential Tree Care Items',
+    'article-2__description': 'The people, places, and things that will keep your trees in tip top shape',
+    'article-3__link': 'https://caseytrees.org/2021/05/closing-out-mental-health-month-with-ideas-for-tree-based-breaks/?utm_source=leaflet&utm_medium=email&utm_campaign=store',
+    'article-3__alt-text': 'the sun shining through trees',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/05/mental-health-month-break-2021-email.png',
+    'article-3__heading': 'Closing Out Mental Health Month with Ideas for Tree-Based Breaks',
+    'article-3__description': 'To close out Mental Health Month, we’re sharing some tree and nature based ideas for a brain break.',
+    'banner-b__link': 'https://casey-trees.myshopify.com/',
     'banner-b__alt-text': "The 13th Annual Tree Report Card: The State of DC's Trees in 2020",
-    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/05/trc2020_leaflet.png',
-    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/05/trc2020_leaflet.png',
+    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_browse-our-new-store.png',
+    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_browse-our-new-store-mobile.png',
     'banner-b__text-1': 'TRC', 
     'banner-b__text-2': '2020',
 }
 
-NVP = NVP_20210510
+NVP = NVP_20210524
 #
 
 

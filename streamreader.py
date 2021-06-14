58a7ad3686cb32f874ef6cd7b939682d0e110262
TKhosla <tkhosla@caseytrees.org>
2021-06-14 11:00:50 -0400

    2021 june 14

--- streamreader.py
+++ streamreader.py
@@ -1,38 +1,38 @@
 import sys
 
 
-NVP_20210524 = {
-    'preheader': 'and Essential Tree Care Items.',
-    'article-1__link': 'https://caseytrees.org/2021/05/fostering-stewardship-on-kingman-heritage-islands/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
-    'article-1__alt-text': 'filming with our education team at Kingman and Heritage Islands',
-    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/05/css-kingman-videos-email.png',
-    'article-1__heading': 'Fostering Stewardship on Kingman & Heritage Islands',
-    'article-1__description': 'Our work on the Community Stormwater Solutions Grant and our work collaborating to improve  our waterways',
+NVP_20210601 = {
+    'preheader': "and don't forget Dad!",
+    'article-1__link': 'https://caseytrees.org/2021/06/need-a-fathers-day-gift/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
+    'article-1__alt-text': 'dads and kids',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/06/fathers-day-21-email.png',
+    'article-1__heading': 'Need a Father’s Day Gift?',
+    'article-1__description': 'Symbolically adopting a tree makes an easy, everlasting gift',
     'banner-ss__link-left' : 'https://caseytrees.org/take-action/water/',
-    'banner-ss__alt-left' : 'Weekly Watering Alerts: Must Water',
-    'banner-ss__image-left' : 'https://caseytrees.org/wp-content/uploads/2021/05/weekly-watering-alert_must-water-mobile.jpg',
+    'banner-ss__alt-left' : 'Weekly Watering Alerts: Watering Optional',
+    'banner-ss__image-left' : 'https://caseytrees.org/wp-content/uploads/2021/05/weekly-watering-alert_optional-mobile.jpg',
     'banner-ss__link-right' : 'https://caseytrees.org/2021/05/cicada-resources/?utm_source=leaflet&utm_medium=email&utm_campaign=treecare',
     'banner-ss__alt-right' : 'Cicada Updates',
     'banner-ss__image-right' : 'https://caseytrees.org/wp-content/uploads/2021/05/cicada_leaflet.png',
-    'article-2__link': 'https://caseytrees.org/2021/05/essential-tree-care-items/?utm_source=leaflet&utm_medium=email&utm_campaign=treecare',
-    'article-2__alt-text': 'Watering a young tree with a big bucket!',
-    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2020/04/water-resplonsive.jpg',
-    'article-2__heading': 'Essential Tree Care Items',
-    'article-2__description': 'The people, places, and things that will keep your trees in tip top shape',
-    'article-3__link': 'https://caseytrees.org/2021/05/closing-out-mental-health-month-with-ideas-for-tree-based-breaks/?utm_source=leaflet&utm_medium=email&utm_campaign=store',
-    'article-3__alt-text': 'the sun shining through trees',
-    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/05/mental-health-month-break-2021-email.png',
-    'article-3__heading': 'Closing Out Mental Health Month with Ideas for Tree-Based Breaks',
-    'article-3__description': 'To close out Mental Health Month, we’re sharing some tree and nature based ideas for a brain break.',
-    'banner-b__link': 'https://casey-trees.myshopify.com/',
-    'banner-b__alt-text': "The 13th Annual Tree Report Card: The State of DC's Trees in 2020",
-    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_browse-our-new-store.png',
-    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/03/Leaflet-ad_browse-our-new-store-mobile.png',
+    'article-2__link': 'https://caseytrees.org/2021/06/these-trees-get-you-100/?utm_source=leaflet&utm_medium=email&utm_campaign=rebate',
+    'article-2__alt-text': 'row houses and the tree crowns',
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/06/tree-rebate-email.png',
+    'article-2__heading': 'These Trees Get You $100',
+    'article-2__description': 'Among countless other benefits from Oaks and more',
+    'article-3__link': 'https://caseytrees.org/2021/06/ornamental-trees-well-plant-for-free/?utm_source=leaflet&utm_medium=email&utm_campaign=riversmarthomes',
+    'article-3__alt-text': 'an ornalmental dogwood',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/05/dogwood-ornamental-rsh-email.png',
+    'article-3__heading': "Trees We’ll Plant for Free",
+    'article-3__description': 'So many choices, so little time. These showstoppers deliver',
+    'banner-b__link': 'https://caseytrees.org/urban-tree-summit-2021/',
+    'banner-b__alt-text': "Early Access Tickets: Montgomery Parks and Casey Trees Present the Urban Tree Summit",
+    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/06/Leaflet-ad_uts-eb.png',
+    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/06/Leaflet-ad_uts-eb-mobile.png',
     'banner-b__text-1': 'TRC', 
     'banner-b__text-2': '2020',
 }
 
-NVP = NVP_20210524
+NVP = NVP_20210601
 #
 
 

5a53e6bd5c66b05e4f6012199bbcb635e91fa471
TKhosla <tkhosla@caseytrees.org>
2021-08-31 12:09:48 -0400

    last day of august 2021

--- notes.py
+++ notes.py
@@ -1781,3 +1781,65 @@ NVP_20210726 = {
     # 'banner-b__text-1': 'follow us on social', 
     # 'banner-b__text-2': 'Social',
 }
+
+
+
+NVP_20210809 = {
+    'preheader': 'Coming Soon to Park Near You: DDOT’s Pop Up Arboreta',
+    'article-1__link': 'https://caseytrees.org/2021/08/ddots-pop-up-arboreta/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
+    'article-1__alt-text': 'a sign posted on an American Elm (ulmus americana) describing its sawtooth leaves',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/08/ddot-arboretum-email.png',
+    'article-1__heading': 'DDOT’s Pop Up Arboreta',
+    'article-1__description': 'And experience it for yourself during our Tree Walk with UFD',
+    'banner-a__link': 'https://caseytrees.org/take-action/water/',
+    'banner-a__alt-text': 'Weekly Watering Alert: Water!',
+    'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/05/weekly-watering-alert_optional.jpg',
+    'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/05/weekly-watering-alert_optional-mobile.jpg',
+    'article-2__link': 'https://caseytrees.org/2021/08/continuing-education-credits-available-at-the-urban-tree-summit/?utm_source=leaflet&utm_medium=email&utm_campaign=greencitiessummit',
+    'article-2__alt-text': 'a stylized gingko and the casey trees and montgomery parks logos',
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/07/uts-2021-email-1.png',
+    'article-2__heading': 'Continuing Education Credits available at the Urban Tree Summit',
+    'article-2__description': 'Grow your knowledge, keep your credentials, and have a great time in September',
+    'article-3__link': 'https://caseytrees.org/2021/08/trees-are-feeling-the-heat/?utm_source=leaflet&utm_medium=email&utm_campaign=treecare',
+    'article-3__alt-text': 'Dry leaves in the summer hear',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/08/heat-stress-email.png',
+    'article-3__heading': 'Trees are Feeling the Heat',
+    'article-3__description': 'Lack of water is not the only summertime woe for urban trees.',
+    'banner-b__link': 'https://caseytrees.org/urban-tree-summit-2021/',
+    'banner-b__alt-text': "Early Access Tickets: Montgomery Parks and Casey Trees Present the Urban Tree Summit",
+    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/08/Leaflet-ad_uts-eb-lastcall.png',
+    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/08/Leaflet-ad_uts-eb-lastcall-mobile.png',
+}
+
+NVP = NVP_20210809
+#
+
+
+NVP_20210809 = {
+    'preheader': 'and the hottest accessory for back to school cool',
+    'article-1__link': 'https://caseytrees.org/2021/08/olmsted-200/?utm_source=leaflet&utm_medium=email&utm_campaign=greencitiessummit',
+    'article-1__alt-text': 'the olmsted 200 logo in front of beautiful canopy',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/08/olmsted-email.png',
+    'article-1__heading': 'Olmsted 200',
+    'article-1__description': 'Casting a spotlight on Frederick Law Olmsted’s 200th birthday and an Urban Tree Summit Sponsor',
+    'banner-a__link': 'https://caseytrees.org/take-action/water/',
+    'banner-a__alt-text': 'Weekly Watering Alert: Do not water.',
+    'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/05/weekly-watering-alert_must-water.jpg',
+    'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/05/weekly-watering-alert_must-water-mobile.jpg',
+    'article-2__link': 'https://caseytrees.org/2021/08/trees-for-our-changing-climate/?utm_source=leaflet&utm_medium=email&utm_campaign=riversmarthomes',
+    'article-2__alt-text': 'the serrated leaves of a river birch',
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/08/river-birch-email.png',
+    'article-2__heading': 'Trees for Our Changing Climate',
+    'article-2__description': "Trees that prefer water when it's either feast or famine",
+    'article-3__link': 'https://caseytrees.org/2021/08/back-to-school-styles/?utm_source=leaflet&utm_medium=email&utm_campaign=store',
+    'article-3__alt-text': 'people using casey trees species guide',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/08/schools-out-email.png',
+    'article-3__heading': 'Back To School Styles',
+    'article-3__description': 'From masks to totes to hats and tees we’ve got everything to outfit you from head to toe.',
+    'banner-b__link': 'https://caseytrees.org/urban-tree-summit-2021/',
+    'banner-b__alt-text': "Tickets: Montgomery Parks and Casey Trees Present the Urban Tree Summit",
+    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/08/Leaflet-ad_uts-tickets.png',
+    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/08/Leaflet-ad_uts-tickets-mobile.png',
+}
+
+NVP = NVP_20210809

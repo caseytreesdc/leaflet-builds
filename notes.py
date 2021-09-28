a0944acd8bd295f3ce3d7c9df08862949bc6e08f
TKhosla <tkhosla@caseytrees.org>
2021-09-28 10:10:15 -0400

    end of sep 2021

--- notes.py
+++ notes.py
@@ -1904,3 +1904,33 @@ NVP_20210913 = {
 
 NVP = NVP_20210913
 #
+
+
+NVP_20210913 = {
+    'preheader': '',
+    'article-1__link': 'https://caseytrees.org/2021/09/in-person-fall-events-are-back/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
+    'article-1__alt-text': 'a pile of shovels!',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/09/summer-treecare-wrap-email.png',
+    'article-1__heading': 'In Person Fall Events are Back!',
+    'article-1__description': 'Register ASAP, our limited number of spots will go quick!',
+    'banner-a__link': 'https://caseytrees.org/take-action/water/',
+    'banner-a__alt-text': 'Weekly Watering Alert: Water',
+    'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/05/weekly-watering-alert_must-water.jpg',
+    'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/05/2021-Watering-Alerts_must-water-mobile.png',
+    'article-2__link': 'https://caseytrees.org/2021/09/make-fall-your-best-season-yet-with-casey-trees/?utm_source=leaflet&utm_medium=email&utm_campaign=riversmarthomes',
+    'article-2__alt-text': 'a kid playing in the leaves',
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/09/fall-email.png',
+    'article-2__heading': 'Make Fall Your Best Season Yet with Casey Trees',
+    'article-2__description': 'We have everything you need for the best show in town - trees!',
+    'article-3__link': 'https://caseytrees.org/2021/09/celebrating-national-hispanic-heritage-month/?utm_source=leaflet&utm_medium=email&utm_campaign=staff',
+    'article-3__alt-text': 'Alex Palacios setting up a watering bag',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/09/alex-email.png',
+    'article-3__heading': 'Celebrating Hispanic Heritage Month',
+    'article-3__description': 'Inspiring the Next Generation of Tree Stewards',
+    'banner-b__link': 'https://caseytrees.org/guia-para-el-guardabosques-junior/',
+    'banner-b__alt-text': "guias para el guardabosques junior",
+    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/09/Leaflet-button-National-Hispanic-Heritage-Month_desktop.jpg',
+    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/09/Leaflet-button-National-Hispanic-Heritage-Month_mobile.jpg',
+}
+
+NVP = NVP_20210913
--- streamreader.py
+++ streamreader.py
@@ -1,34 +1,34 @@
 import sys
 
 
-NVP_20210913 = {
-    'preheader': 'They’ll go fast people!',
-    'article-1__link': 'https://caseytrees.org/2021/09/in-person-fall-events-are-back/?utm_source=leaflet&utm_medium=email&utm_campaign=programming',
-    'article-1__alt-text': 'a pile of shovels!',
-    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/09/summer-treecare-wrap-email.png',
-    'article-1__heading': 'In Person Fall Events are Back!',
-    'article-1__description': 'Register ASAP, our limited number of spots will go quick!',
+NVP_20210927 = {
+    'preheader': 'The Urban Forestry Division has a treasure trove of maps for residents',
+    'article-1__link': 'https://caseytrees.org/2021/09/casey-trees-headquarters-street-tree-tour/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
+    'article-1__alt-text': 'casey trees headquarters',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/09/street-tree-tour-email.png',
+    'article-1__heading': 'Casey Trees Headquarters Street Tree Tour',
+    'article-1__description': 'The Urban Forestry Division has been hard at work outside and creating valuable resources for the public!',
     'banner-a__link': 'https://caseytrees.org/take-action/water/',
     'banner-a__alt-text': 'Weekly Watering Alert: Water',
     'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/05/weekly-watering-alert_must-water.jpg',
     'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/05/2021-Watering-Alerts_must-water-mobile.png',
-    'article-2__link': 'https://caseytrees.org/2021/09/make-fall-your-best-season-yet-with-casey-trees/?utm_source=leaflet&utm_medium=email&utm_campaign=riversmarthomes',
-    'article-2__alt-text': 'a kid playing in the leaves',
-    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/09/fall-email.png',
-    'article-2__heading': 'Make Fall Your Best Season Yet with Casey Trees',
-    'article-2__description': 'We have everything you need for the best show in town - trees!',
-    'article-3__link': 'https://caseytrees.org/2021/09/celebrating-national-hispanic-heritage-month/?utm_source=leaflet&utm_medium=email&utm_campaign=staff',
-    'article-3__alt-text': 'Alex Palacios setting up a watering bag',
-    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/09/alex-email.png',
-    'article-3__heading': 'Celebrating Hispanic Heritage Month',
-    'article-3__description': 'Inspiring the Next Generation of Tree Stewards',
+    'article-2__link': 'https://caseytrees.org/2021/09/urban-tree-summit-2021-recap-back-and-better-than-before/?utm_source=leaflet&utm_medium=email&utm_campaign=urbantreesummit',
+    'article-2__alt-text': 'a crowd gathered at langdon park',
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/09/uts-recap-2021-email.png',
+    'article-2__heading': 'Urban Tree Summit 2021 Recap: Back and Better than Before',
+    'article-2__description': 'Hear our favorite parts of each session',
+    'article-3__link': 'https://caseytrees.org/2021/09/treewise-2021-summer-recap/?utm_source=leaflet&utm_medium=email&utm_campaign=treewise',
+    'article-3__alt-text': 'what i enjoy outside is the wonderful trees and leafs - a drawing of a red maple with markers',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/09/treewise-summer-2021-recap-email.png',
+    'article-3__heading': 'TreeWise 2021 Summer Recap',
+    'article-3__description': 'We were so thankful to be back in person with the next generation of tree stewards',
     'banner-b__link': 'https://caseytrees.org/guia-para-el-guardabosques-junior/',
     'banner-b__alt-text': "guias para el guardabosques junior",
     'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/09/Leaflet-button-National-Hispanic-Heritage-Month_desktop.jpg',
     'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/09/Leaflet-button-National-Hispanic-Heritage-Month_mobile.jpg',
 }
 
-NVP = NVP_20210913
+NVP = NVP_20210927
 #
 
 

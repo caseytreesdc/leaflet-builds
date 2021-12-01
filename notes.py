af37b763057f9425227102ec13e6532ffe7e60e9
TKhosla <tkhosla@caseytrees.org>
2021-12-01 12:05:32 -0500

    eoy 2021 1

--- notes.py
+++ notes.py
@@ -2049,3 +2049,34 @@ NVP_20211115 = {
     'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/11/Leaflet-button_climate-crisis-conf-desktop.png',
     'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/11/Leaflet-button_climate-crisis-conf-mobile.png',
 }
+
+
+NVP_20211115 = {
+    'preheader': "You give, they match. We’re trying to raise $5,000 for DC’s trees - will you help?",
+    'article-1__link': 'https://caseytrees.org/2021/11/tomorrow-your-chance-to-make-a-difference-for-dcs-trees/?utm_source=leaflet&utm_medium=email&utm_campaign=eoy',
+    'article-1__alt-text': 'shade trees with building peeking through',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/06/medium-shade-tree-email.png',
+    'article-1__heading': 'Tomorrow. Your Chance to Make a Difference for DC’s Trees.',
+    'article-1__description': 'You give, they match, DC’s trees wins. #GivingTuesday is tomorrow!',
+    'banner-a__link': 'https://caseytrees.org/education/urban-forestry-fellowship/',
+    'banner-a__alt-text': 'Urban Forestry Student Scholarship',
+    'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/11/Leaflet-ad_gca_v2.png',
+    'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/11/Leaflet-ad_gca-mobile-2.png',
+    'article-2__link': 'https://caseytrees.org/2021/11/introducingthe-urban-forest-preservation-authority-amendment-act-of-2021/?utm_source=leaflet&utm_medium=email&utm_campaign=advocacy',
+    'article-2__alt-text': "a huge scarlet oak special tree",
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/11/dcist-trees-email.png',
+    'article-2__heading': 'Introducing…the Urban Forest Preservation Authority Amendment Act Of 2021',
+    'article-2__description': 'We’re partnering with Councilmembers Cheh and Allen to strengthen protections for our biggest trees',
+    'article-3__link': 'https://caseytrees.org/2021/11/we-get-by-with-a-little-help-from-our-friends/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
+    'article-3__alt-text': 'a fall tree lined street leading to one of DCs many roundabouts',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/11/ufd-thankful-email.png',
+    'article-3__heading': "We Get By with a Little Help from Our Friends",
+    'article-3__description': 'This year, we’re especially thankful for our partner in urban forestry',
+    'banner-b__link': 'http://www.facebook.com/pages/Casey-Trees/58793928339',
+    'banner-b__alt-text': "follow us on facebook",
+    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/01/Leaflet-ad_follow-us-on-social.png',
+    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/01/Leaflet-ad_follow-us-on-social-mobile.png',
+}
+
+NVP = NVP_20211115
+#
--- streamreader.py
+++ streamreader.py
@@ -1,34 +1,34 @@
 import sys
 
 
-NVP_20211115 = {
-    'preheader': "and a homegrown (literally) Black Friday",
-    'article-1__link': 'https://caseytrees.org/2021/11/support-homegrown-change-this-year-give-to-casey-trees-for-givingtuesday/?utm_source=leaflet&utm_medium=email&utm_campaign=eoy',
-    'article-1__alt-text': 'under the tree canopy',
-    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/04/tree-walk-2021-email.png',
-    'article-1__heading': 'Support Homegrown Change this Year. Give to Casey Trees for #GivingTuesday',
-    'article-1__description': 'Just as every tree planted makes a difference, every dollar donated on #GivingTuesday next week makes a difference. Make your contribution at caseytrees.org/give early or on Tuesday, November 30.',
-    'banner-a__link': 'https://caseytrees.org/education/urban-forestry-fellowship/',
-    'banner-a__alt-text': 'Urban Forestry Student Scholarship',
-    'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/11/Leaflet-ad_gca_v2.png',
-    'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/11/Leaflet-ad_gca-mobile-2.png',
-    'article-2__link': 'https://caseytrees.org/2021/11/introducingthe-urban-forest-preservation-authority-amendment-act-of-2021/?utm_source=leaflet&utm_medium=email&utm_campaign=advocacy',
-    'article-2__alt-text': "a huge scarlet oak special tree",
-    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/11/dcist-trees-email.png',
-    'article-2__heading': 'Introducing…the Urban Forest Preservation Authority Amendment Act Of 2021',
-    'article-2__description': 'We’re partnering with Councilmembers Cheh and Allen to strengthen protections for our biggest trees',
-    'article-3__link': 'https://caseytrees.org/2021/11/we-get-by-with-a-little-help-from-our-friends/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
-    'article-3__alt-text': 'a fall tree lined street leading to one of DCs many roundabouts',
-    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/11/ufd-thankful-email.png',
-    'article-3__heading': "We Get By with a Little Help from Our Friends",
-    'article-3__description': 'This year, we’re especially thankful for our partner in urban forestry',
+NVP_20211129 = {
+    'preheader': "You give, they match. We’re trying to raise $5,000 for DC’s trees - will you help?",
+    'article-1__link': 'https://caseytrees.org/2021/11/tomorrow-your-chance-to-make-a-difference-for-dcs-trees/?utm_source=leaflet&utm_medium=email&utm_campaign=eoy',
+    'article-1__alt-text': 'shade trees with building peeking through',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/06/medium-shade-tree-email.png',
+    'article-1__heading': 'Tomorrow. Your Chance to Make a Difference for DC’s Trees.',
+    'article-1__description': 'You give, they match, DC’s trees win. #GivingTuesday is tomorrow!',
+    'banner-a__link': 'https://caseytrees.org/give',
+    'banner-a__alt-text': 'Give on giving tuesday',
+    'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/11/Giving-Tuesday_gt-email-leaf.png',
+    'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/11/Giving-Tuesday_gt-email-leaf.png',
+    'article-2__link': 'https://caseytrees.org/2021/11/casey-trees-online-advocacy-portal/?utm_source=leaflet&utm_medium=email&utm_campaign=advocacy',
+    'article-2__alt-text': "a logo of a laptop and a loudspeaker",
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/11/advocacy-portal_emal.png',
+    'article-2__heading': 'Casey Trees Online Advocacy Resources',
+    'article-2__description': 'Classes on everything you need to demystify DC government and speak for trees - in your own home on your own time',
+    'article-3__link': 'https://caseytrees.org/2021/11/from-casey-trees-to-the-urban-forestry-division/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
+    'article-3__alt-text': 'Maddy McPhee: Urban Forester Ward 1',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/11/maddy-ufd-email.png',
+    'article-3__heading': "We Love Working with Old Friends",
+    'article-3__description': 'One of the reasons we work so well with UFD? Their arborist is a former employee!',
     'banner-b__link': 'http://www.facebook.com/pages/Casey-Trees/58793928339',
     'banner-b__alt-text': "follow us on facebook",
     'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/01/Leaflet-ad_follow-us-on-social.png',
     'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/01/Leaflet-ad_follow-us-on-social-mobile.png',
 }
 
-NVP = NVP_20211115
+NVP = NVP_20211129
 #
 
 

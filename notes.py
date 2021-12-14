28c4412109d949c3e936cadc294a4818bb0fccab
TKhosla <tkhosla@caseytrees.org>
2021-12-13 21:10:53 -0500

    eoy dec 13 2021

--- notes.py
+++ notes.py
@@ -2110,3 +2110,30 @@ NVP_20211129 = {
 }
 
 NVP = NVP_20211129
+
+NVP_2021206 = {
+    'preheader': "and a welcome to the newest DDOT Director",
+    'article-1__link': 'https://caseytrees.org/2021/12/its-the-little-things/?utm_source=leaflet&utm_medium=email&utm_campaign=eoy',
+    'article-1__alt-text': 'planting crew planting pines',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/12/little-things-leaflet-email.png',
+    'article-1__heading': 'It’s the Little Things',
+    'article-1__description': 'One small tree for a yard, one giant leap for urban kind',
+    'banner-a__link': 'https://connect.clickandpledge.com/w/Form/446220bb-4a7a-4225-8b33-39cd133877c1?trk=EOYEmail5&utm_source=listemail&utm_medium=email-5&utm_campaign=eoy2021',
+    'banner-a__alt-text': 'Support Casey Trees this giving season. Donate Now',
+    'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/12/Leaflet-button_give-desktop.png',
+    'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/12/Leaflet-button_give-mobile.png',
+    'article-2__link': 'https://caseytrees.org/2021/12/congratulations-new-ddot-director-everett-lott/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
+    'article-2__alt-text': "new DDOT director Everett Lott",
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/12/dott-dir-new-email.png',
+    'article-2__heading': 'Congratulations New DDOT Director Everett Lott',
+    'article-2__description': 'We are so excited to work with Director Lott and all DDOT partners to continue to grow, enhance, and protect DC’s tree canopy. ',
+    'article-3__link': 'https://caseytrees.org/2021/12/calling-all-urban-forestry-students-2/?utm_source=leaflet&utm_medium=email&utm_campaign=gcascholarship',
+    'article-3__alt-text': 'inspecting tree leaves',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/12/gca-final-2-email.png',
+    'article-3__heading': "Calling All Urban Forestry Students",
+    'article-3__description': 'Apply for our $7,500 fellowship or forward to a friend who would be interested!',
+    'banner-b__link': 'https://caseytrees.org/education/urban-forestry-fellowship/',
+    'banner-b__alt-text': 'Urban Forestry Student Scholarship',
+    'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/11/Leaflet-ad_gca_v2.png',
+    'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/11/Leaflet-ad_gca-mobile-2.png',
+}
--- streamreader.py
+++ streamreader.py
@@ -1,34 +1,34 @@
 import sys
 
 
-NVP_2021206 = {
-    'preheader': "and a welcome to the newest DDOT Director",
-    'article-1__link': 'https://caseytrees.org/2021/12/its-the-little-things/?utm_source=leaflet&utm_medium=email&utm_campaign=eoy',
-    'article-1__alt-text': 'planting crew planting pines',
-    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/12/little-things-leaflet-email.png',
-    'article-1__heading': 'It’s the Little Things',
-    'article-1__description': 'One small tree for a yard, one giant leap for urban kind',
+NVP_2021213 = {
+    'preheader': "One of the best investments in urban infrastructure you can make",
+    'article-1__link': 'https://caseytrees.org/2021/12/all-i-want-for-christmas-is-tree-memes/?utm_source=leaflet&utm_medium=email&utm_campaign=eoy',
+    'article-1__alt-text': '',
+    'article-1__image': 'https://caseytrees.org/wp-content/uploads/2021/12/12.13_memes-leaflet-email.jpg',
+    'article-1__heading': 'All I Want For Christmas is Tree Memes',
+    'article-1__description': 'An updated look at our favorite and most popular memes',
     'banner-a__link': 'https://connect.clickandpledge.com/w/Form/446220bb-4a7a-4225-8b33-39cd133877c1?trk=EOYEmail5&utm_source=listemail&utm_medium=email-5&utm_campaign=eoy2021',
     'banner-a__alt-text': 'Support Casey Trees this giving season. Donate Now',
     'banner-a__image-browser': 'https://caseytrees.org/wp-content/uploads/2021/12/Leaflet-button_give-desktop.png',
     'banner-a__image-responsive': 'https://caseytrees.org/wp-content/uploads/2021/12/Leaflet-button_give-mobile.png',
-    'article-2__link': 'https://caseytrees.org/2021/12/congratulations-new-ddot-director-everett-lott/?utm_source=leaflet&utm_medium=email&utm_campaign=citypartner',
-    'article-2__alt-text': "new DDOT director Everett Lott",
-    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/12/dott-dir-new-email.png',
-    'article-2__heading': 'Congratulations New DDOT Director Everett Lott',
-    'article-2__description': 'We are so excited to work with Director Lott and all DDOT partners to continue to grow, enhance, and protect DC’s tree canopy. ',
-    'article-3__link': 'https://caseytrees.org/2021/12/calling-all-urban-forestry-students-2/?utm_source=leaflet&utm_medium=email&utm_campaign=gcascholarship',
-    'article-3__alt-text': 'inspecting tree leaves',
-    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/12/gca-final-2-email.png',
-    'article-3__heading': "Calling All Urban Forestry Students",
-    'article-3__description': 'Apply for our $7,500 fellowship or forward to a friend who would be interested!',
+    'article-2__link': 'https://caseytrees.org/2021/12/the-gift-that-keeps-on-giving-urban-trees/?utm_source=leaflet&utm_medium=email&utm_campaign=eoy',
+    'article-2__alt-text': "people enjoying a walk under a canopy on both sides of a path",
+    'article-2__image': 'https://caseytrees.org/wp-content/uploads/2021/07/summer-care-2021-email.png',
+    'article-2__heading': 'The Gift that Keeps on Giving: Urban Trees',
+    'article-2__description': 'From improving air quality, lower temperatures, absorbing stormwater and providing mental health benefits, trees are not just nice to have or nice to look at.',
+    'article-3__link': 'https://caseytrees.org/2021/12/one-gift-to-rule-them-all/?utm_source=leaflet&utm_medium=email&utm_campaign=eoy',
+    'article-3__alt-text': 'A graphic of DC with the whitehouse',
+    'article-3__image': 'https://caseytrees.org/wp-content/uploads/2021/12/12.13-story-leaflet-email.jpg',
+    'article-3__heading': "For The Person That Has Everything",
+    'article-3__description': "One Gift to Rule Them All",
     'banner-b__link': 'https://caseytrees.org/education/urban-forestry-fellowship/',
     'banner-b__alt-text': 'Urban Forestry Student Scholarship',
     'banner-b__image-browser': 'https://caseytrees.org/wp-content/uploads/2020/11/Leaflet-ad_gca_v2.png',
     'banner-b__image-responsive': 'https://caseytrees.org/wp-content/uploads/2020/11/Leaflet-ad_gca-mobile-2.png',
 }
 
-NVP = NVP_2021206
+NVP = NVP_2021213
 #
 
 
